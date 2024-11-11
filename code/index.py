9e273d52580ee01068872ededa5e78b3e4eab281
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-11-11 11:56:23 -0300

    remake

--- code/index.py
+++ code/index.py
@@ -71,11 +71,10 @@ class Browser:
             p.append((*[data for data in info if data != ''\
                 and data[:3] != 'com' \
                     and data[:3] != 'apo'],))
+            
+        self.driver.quit()
         return p
     
-    def close(self):
-        self.driver.quit()
-
 class Email:
     PATH_MESSAGE = Path(__file__).parent / 'src' / 'base_message.html'
 
@@ -85,16 +84,28 @@ class Email:
 
         self.smtp_username = os.getenv("EMAIL_SENDER","")
         self.smtp_password = os.getenv("PASSWRD_SENDER","")
+
+        self.ref_cor = {
+            'IPCA': 'lightyellow',
+            'RENDA': 'lightblue',
+            'EDUCA': 'lightgreen',
+            'PREFIXADO': 'lightcyan',
+            'SELIC': 'lightcoral'
+        }
         pass
 
     def create_message(self, move: list[tuple]) -> str:
         format_data = []
-        for index, item in enumerate(move, 1):
+        for item in move:
+            y = list(item)
+            y[1] = re.sub(r"\d", "", item[1])
+            item = tuple(y)
+
             item_str = ''.join(
                 [f'<td> {data} </td>' for data in item[1:]]
             )
 
-            color = 'lightyellow' if index % 2 == 0 else 'lightblue'
+            color = self.get_color(item[1])
             format_data.append(
                  f'<tr style="background-color: {color};"> {item_str} </tr>'
             )
@@ -103,6 +114,13 @@ class Email:
             text_message = file.read()
             return Template(text_message)\
                 .substitute(infos =  ''.join(x for x in format_data))
+        
+    def get_color(self, name_row):
+        for key, color in self.ref_cor.items():
+            if name_row == key:
+                return color
+        return 'white'
+
 
     def send(self, texto_email: str, to: str) -> None:
         mime_multipart = MIMEMultipart()
@@ -121,8 +139,8 @@ class Email:
             server.send_message(mime_multipart)
         
 class DataBase:
-    NOME_DB = 'tesouro_db.sqlite3'
-    FOLDER_DB = resource_path(f'src\\db\\{NOME_DB}')
+    DB_NAME = 'tesouro_db.sqlite3'
+    FOLDER_DB = resource_path(f'src\\db\\{DB_NAME}')
     TABLE_LATE = 'Infos_Late'
     TABLE_OLD = 'Infos_Const'
 
@@ -133,7 +151,7 @@ class DataBase:
         )
 
         self.query_late = (
-            f'SELECT * FROM {self.TABLE_LATE}'
+            f'SELECT * FROM {self.TABLE_LATE} ORDER BY Titulo ASC;'
         )
 
         self.update_late = (
@@ -159,32 +177,116 @@ class DataBase:
         self.cursor = self.connection.cursor()
         pass
 
-    def init(self, contents: list[tuple]):
-        for item in contents:
+    def init(self, infos_site: list[tuple]):
+        for item in infos_site:
             self.cursor.execute(
                 self.insert_late,
                 (item[0], item[1], item[2], item[3], item[4], item[5])
             )
         self.connection.commit()
 
-    def filter_moveless(self, contents: list[tuple]):
+    def query(self) -> list[tuple]:
         self.cursor.execute(
-            self.query_late.format(self.TABLE_LATE)
+            self.query_late
         )
-        lates_moves = self.cursor.fetchall()
+        return self.cursor.fetchall()
+    
+    def insert(self, data_site) -> None:
+         for item in data_site:
+            self.cursor.execute(
+                self.insert_late,
+                (item[0], item[1], item[2], item[3], item[4], item[5])
+            )
+            self.connection.commit()
+    
+    def update(self, move: list[tuple]):
+        for item in move:
+            self.cursor.execute(
+                self.update_late,
+                (item[1], item[2], item[3], item[5], item[6], item[7], item[0])
+            )
+            self.connection.commit()
 
-        filtred_moves = []
-        for move in lates_moves:
-            # print(f'{move} - movimento db')
-            for content in contents:
-                # print(f'{content} - opção do site')
-                if content[0] == move[1]\
-                    and content[2] != move[3]:
-                    filtred_moves.append(
-                        (str(move[0]),) + (re.sub(r"\d", "", content[0]) ,) + content[1:3] + (self.variacao(content[2], move[3]),) + content[3:]
-                    )
-        # print(f'{filtred_moves} - resultado final')
-        return filtred_moves
+    def exit(self):
+        self.cursor.close()
+        self.connection.close()
+
+class Report:
+    def __init__(self) -> None:
+        self.path = resource_path('report.txt')
+        pass
+
+    def is_send(self, to: str):
+        with open(self.path, 'a', encoding= 'utf-8') as file:
+            file.write('\n')
+            file.write('-'*20 + f'Email enviado às {datetime.strftime(datetime.now())} para:\n')
+            file.write(to)
+
+    def is_new(self, unfound: list[tuple]):
+        with open(self.path, 'a', encoding= 'utf-8') as file:
+            file.write('\n')
+            file.write('\n#'*10 + 'Linhas Adcionadas:\n')
+            for row in unfound:
+                file.write(row[1])
+
+    def is_updated(self, outdated: list[tuple]):
+        with open(self.path, 'a', encoding= 'utf-8') as file:
+            file.write('#'*10 + 'Linhas Atualizadas:\n')
+            for row in outdated:
+                file.write(row[1])
+
+class Main:
+    def __init__(self) -> None:
+        self.email = Email()
+        self.db = DataBase()
+        self.report = Report()
+        pass
+
+    def hard_work(self):
+        try:
+            infos_site = Browser().search()
+
+            infos_db = self.db.query()
+
+            moves = self.filter(infos_db, infos_site)
+
+            if moves != []:
+                message = self.email.create_message(moves)
+                for person in json.loads(os.environ['ADDRESSE']):
+                    self.email.send(message, person)
+                    self.report.is_send(person)
+        finally:
+            self.db.exit()
+
+    def filter(self, infos_db: list[tuple], infos_site: list[tuple]):
+        outdated_site = []
+        unfound_site = []
+
+        for data_site in infos_site:
+            # print(f'{data_site}  - opção do site')
+            achado = False
+            for data_db in infos_db:
+                # print(f'{data_db} - movimento db')
+                if data_site[0] == data_db[1]:
+                    achado = True
+
+                    if data_site[2] != data_db[3]:
+                        outdated_site.append(
+                            (str(data_db[0]),) + data_site[0:3] + (self.variacao(data_site[2], data_db[3]),) + data_site[3:]
+                        )
+                    # continue
+
+            if achado == False: unfound_site.append(data_site)
+
+        if unfound_site != []:
+            self.db.insert(unfound_site)
+            self.report.is_new(unfound_site)
+
+        if outdated_site != []:
+            self.db.update(outdated_site)
+            self.report.is_updated(outdated_site)
+
+        return outdated_site + unfound_site
     
     def variacao(self, new_value: str, old_value: str, ):
         values = []
@@ -194,39 +296,9 @@ class DataBase:
         result = ((values[1] - values[0]) / values[0]) * 100
 
         return f'{result:,.2f}% {'▲' if result > 0 else '▽'}'.replace('.',',') 
-
-    def update(self, move: list[tuple]):
-        for item in move:
-            self.cursor.execute(
-                self.update_late.format(
-                    self.TABLE_LATE,
-                ),
-                (item[1], item[2], item[3], item[5], item[6], item[7], item[0])
-            )
-            self.connection.commit()
-
-    def exit(self):
-        self.cursor.close()
-        self.connection.close()
+    
+    def init_db(self):
+        self.db.init(Browser().search())
 
 if __name__ == '__main__':
-    try:
-        browser = Browser()
-        email = Email()
-        db = DataBase()
-        
-        content = browser.search()
-        browser.close()
-
-        move = db.filter_moveless(content)
-
-        if move != []:
-            message = email.create_message(move)
-            for person in json.loads(os.environ['ADDRESSE']):
-                email.send(message, person)
-                print('Email enviado com sucesso para: ' + person)
-            db.update(move)
-    # except Exception as err:
-    finally:
-        db.exit()
-
+    Main().hard_work()
